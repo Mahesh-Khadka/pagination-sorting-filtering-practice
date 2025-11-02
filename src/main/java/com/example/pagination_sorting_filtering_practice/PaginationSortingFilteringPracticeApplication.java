9f6f94f88fcd8a17eb671c15b22877176db5b6c7
Mahesh-Khadka <maheshkhadka438@gmail.com>
2025-11-02 11:36:39 +0545

    Initial commit

---
+++ src/main/java/com/example/pagination_sorting_filtering_practice/PaginationSortingFilteringPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pagination_sorting_filtering_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSortingFilteringPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSortingFilteringPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pagination_sorting_filtering_practice/PaginationSortingFilteringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pagination_sorting_filtering_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSortingFilteringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
